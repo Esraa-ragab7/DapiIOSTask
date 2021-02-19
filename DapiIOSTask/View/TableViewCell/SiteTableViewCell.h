f7b52e01fb7a8cd6c6aba87e5caf6362f07cf5bd
Esraa Azab <sara2_fun2010@yahoo.com>
2021-02-19 22:53:03 +0200

    Add Table View Cell.

---
+++ DapiIOSTask/View/TableViewCell/SiteTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SiteTableViewCell.h
+//  DapiIOSTask
+//
+//  Created by Esraa Mohamed Ragab on 19/02/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SiteTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DapiIOSTask/View/TableViewCell/SiteTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SiteTableViewCell.m
+//  DapiIOSTask
+//
+//  Created by Esraa Mohamed Ragab on 19/02/2021.
+//
+
+#import "SiteTableViewCell.h"
+
+@implementation SiteTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
